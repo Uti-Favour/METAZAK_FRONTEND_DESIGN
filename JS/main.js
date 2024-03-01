9882ddcf38be4104164b4c2152812bd3546476bc
Uti-Favour <utifavour15@gmail.com>
2024-03-01 21:36:56 +0100

    Created a responsive navigation bar

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,21 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 
+    let display_hidden = document.querySelector('.menu_toggle_hidden');
+    let display_block = document.querySelector('.menu_toggle_block');
+    const btn = document.querySelector('.btn');
+
+
+    function OpenNavigationBar(){
+        btn.addEventListener('click' , () => {
+            display_hidden.classList.toggle("display_block");
+            
+
+        });
+
+    }
+
+    
+    OpenNavigationBar();
+    
+
 });
